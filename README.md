55302ed3b00e6637ea8211fe794d9d889a4f3e05
Noël Vissers <noelvisser@gmail.com>
2020-12-04 22:50:20 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,168 @@
+<p align="center">
+  <img alt="Gopher" src="./github/images/icon.png" height="140" />
+  <h3 align="center">site-watcher</h3>
+  <p align="center">A Discord bot that alerts you when (part of) a website changes.</p>
+</p>
+
+---
+
+## Features
+List of features of the site-watcher bot:
+- Add site to watcher
+- Add multiple sites to watcher
+- Remove site from watcher
+- Monitoring specified elements of a site, to not get notified on dynamic elements like ads.
+- Checking on a specified interval (1-60 minutes, default 5).
+- Manually update tracked sites
+- Show list of tracked sites
+- Open source!
+
+## Background
+When I started looking for a new apartment, I got tired of the update emails I got. Every time I got one of these emails, the apartments would already be online for almost a day, so there was no chance I would be close to one of the first ones to respond. The solution: *have something that notifies me instantly when a new apartment is put online.* With this in mind I started looking at sites that monitor this for you. All of them were paid (unless you want 1 update every hour and 5 updates a day). These sites often use email as a way to notify you, which also introduces a decent delay. So I started looking elsewhere. I found a few scripts that can do the same, some even with webhook integration! I also found a few other bots (also for Discord), but they often lacked some features I needed, like being able to select part of a site. As the engineer I am, I started doing some research into how to make this idea a reality and started working on the bot. I hope this can be an example (or fully useful) to someone else!
+
+## Install
+Downloading the project:
+
+1. Create a new discord bot on [discord.com/developers/applications](https://discord.com/developers/applications). A tutorial can be found [here](https://discordpy.readthedocs.io/en/latest/discord.html).   
+2. Make sure you have [git](https://git-scm.com/downloads) and [nodejs](https://nodejs.org/en/) installed.
+3. Right click where you want the project folder and select `Git Bash Here`
+4. Use the command `clone https://github.com/noelvissers/site-watcher.git` to clone the repository.
+5. Open `cmd.bat` in the repository folder
+6. Type `npm install` and press `enter`.
+
+Configuring the bot:
+
+1. Open the `.env` file.
+2. Add your discord bot token after `DISCORDJS_BOT_TOKEN=`. You can get this token from [discord.com/developers/applications](https://discord.com/developers/applications).
+3. Add the channel ID from the channel you want the update notifications in after `DISCORDJS_TEXTCHANNEL_ID=`. You can get this ID by right clicking the channel in discord and selecting `Copy ID`.  Make sure `Developer Mode` is on by going to `Settings → Appearance → Advanced → Developer Mode → ON`.
+4. If you want to change the prefix (default `!`), you can change it in the `./src/monitor.js` file.
+
+For starting and using the bot, see [Usage](#Usage).
+
+## Usage
+The simplest method to monitor a site:
+1. Invite the bot to your Discord server by replacing `123456789012345678` in the following link with your bot's client id: `https://discord.com/oauth2/authorize?client_id=123456789012345678&scope=bot&permissions=8`. Make sure the bot has permission to post in this channel.
+1. Open the command line in the cloned repository folder by opening the `cmd.bat` file.
+2. Start the bot by typing `npm start`.
+3. In Discord (the bot should now be online), add a website with the `!add <URL>` command.
+4. Done! The added site is now being monitored.   
+<sub>By default, the watch interval for every website is 5 minutes, but you can easily change this with the `!interval` command followed by the interval in minutes.</sub>
+
+For all other options, see [Commands](#Commands).
+
+## Commands
+### `!help`
+Show all the available commands.
+
+**Parameters**
+None.
+
+**Example**
+`!help` This will show all the available commands:
+
+---
+
+### `!add <URL> "<CSS SELECTOR>"`
+Adds a website to the list.
+
+**Parameters**   
+Required:   
+`URL` The URL of the site you want to track.   
+
+Optional:   
+`"CSS SELECTOR"` The part of the site you want to track. (By default the \<head\> of the site is tracked).   
+<sub>**Make sure to use double quotation marks when using this parameter.**   
+In Chrome, this can be obtained by right clicking the part of the site you want to track and selecting: `Inspect`. After this you see the element highlighted in the developer view. You can right click on the element and select `Copy → Copy selector`. </sub>
+
+**Example**
+`!add https://google.com/` This tracks changes on https://google.com/.   
+<sub>Note that some sites, including Google.com have dynamic elements (like ads) that cause a change every time its checked. To make sure this is filtered out, use the css selector parameter.</sub>   
+
+`!add https://example.com/ "body > div > h1"` This tracks changes in header 1 of the site https://example.com/:
+
+---
+
+### `!remove <NUMBER>`
+Removes a website from the list.
+
+**Parameters**   
+Required:   
+`NUMBER` The number of the site you want to remove. Use `!list` to see the number of the site(s).   
+
+**Example**
+`!remove 1` This removes the first site in the list (`!list`):
+
+---
+
+### `!list`
+Shows the list of websites being watched.
+
+**Parameters**   
+None.
+
+**Example**
+`!list` This shows the list of websites being watched:
+
+---
+
+### `!update`
+Manually updates the sites that are being watched.
+
+**Parameters**   
+None.
+
+**Example**
+`!update` This manually updates the sites that are being watched:
+<sub>If a site is updated, it will push the standard update message to the default update channel.</sub>
+
+---
+
+### `!interval <MINUTES>`
+Set the interval/refresh rate of the watcher. Default `5` minutes.
+
+**Parameters**
+`MINUTES` The interval in minutes (minimum of 1, maximum of 60).
+
+**Example**
+`!interval 10` Sets the interval to 10 minutes:
+
+---
+
+### `!start`
+Start the watcher with the specified interval (default `ON` with interval of `5` minutes). 
+<sub>This uses [cron](https://www.npmjs.com/package/cron).</sub>
+
+**Parameters**
+None.
+
+**Example**
+`!start` This starts the watcher with the specified interval:
+
+---
+
+### `!stop`
+Stops the watcher from automatically checking the tracked websites. Watcher can be resumed with `!start`.
+
+**Parameters**
+None.
+
+**Example**
+`!stop` This stops the watcher from automatically checking the tracked websites:
+
+---
+
+### `!status`
+Checks if the site watcher is running.
+
+**Parameters**
+None.
+
+**Example**
+`!status` This checks if the site watcher is running:
+
+
+## Contribute
+Im currently not updating the bot with new features, however I'll try to fix bugs that are reported. If you'd like you can add new features or fix bugs by forking the repository and creating a pull request.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ src/monitor.js
@@ -0,0 +1,254 @@
+/**
+ * @author Noël Vissers
+ * @project Site Watcher
+ * @version 1.0.0
+ * 
+ * Invite link: https://discord.com/oauth2/authorize?client_id=123456789012345678&scope=bot&permissions=8
+ * (replace 123456789012345678 with your client id from https://discord.com/developers/applications)
+ * Start: npm start
+ */
+
+require('dotenv').config();
+
+const Discord = require('discord.js');
+var client = new Discord.Client();
+const CronJob = require('cron').CronJob;
+const CronTime = require('cron').CronTime;
+const got = require('got');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+var crypto = require('crypto');
+const fs = require('fs-extra');
+
+const PREFIX = '!';
+var regexp = /[^\s"]+|"([^"]*)"/gi;
+const file = './src/sites.json';
+var sitesToMonitor = [];
+const settingsFile = './src/settings.json';
+var cronTime = { interval: 5 };
+
+client.on('ready', () => {
+  var tempJson = fs.readJSONSync(file);
+  console.log(tempJson);
+  sitesToMonitor = [...tempJson];
+
+  tempJson = fs.readJSONSync(settingsFile);
+  console.log(tempJson);
+  cronTime = tempJson;
+
+  cronUpdate.setTime(new CronTime(`0 */${cronTime.interval} * * * *`));
+  cronUpdate.start();
+  console.log(`[${client.user.tag}] Ready...\n[${client.user.tag}] Running an interval of ${cronTime.interval} minute(s).`);
+})
+
+client.on('message', (message) => {
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+  var args = [];
+  console.log(`[${message.author.tag}]: ${message.content}`);
+  const argsTemp = message.content.slice(PREFIX.length).trim();
+
+  do {
+    var match = regexp.exec(argsTemp);
+    if (match != null) {
+      args.push(match[1] ? match[1] : match[0]);
+    }
+  } while (match != null);
+
+  console.log(args);
+  const CMD_NAME = args.shift().toLowerCase();
+
+  switch (CMD_NAME.toUpperCase()) {
+    case "HELP":
+      {
+        var embed = new Discord.MessageEmbed();
+        embed.setTitle("Commands");
+        embed.setColor('0x6058f3');
+        embed.addField('\`!help\`', 'Show all commands.');
+        embed.addField('\`!add <URL> "<CSS SELECTOR>"\`', 'Add site to monitor with optional CSS selector.');
+        embed.addField('\`!remove <NR>\`', 'Remove site from list.');
+        embed.addField('\`!list\`', 'Show list of added sites.');
+        embed.addField('\`!update\`', 'Manually update sites.');
+        embed.addField('\`!interval\`', 'Set update interval, default \`5\`.');
+        embed.addField('\`!start\`', 'Start automatic monitoring on set interval, default \`on\`.');
+        embed.addField('\`!stop\`', 'Stop monitoring.');
+        embed.addField('\`!status\`', 'Show monitoring status.');
+        message.channel.send(embed);
+      } break;
+    case "ADD":
+      {
+        if (args.length === 0) return message.channel.send('Usage: `!add <URL> (<CSS SELECTOR>)`');
+        var url = args[0];
+        var selector = 'head';
+        if (args[1]) {
+          selector = args[1];
+        }
+
+        var site = {
+          id: url.split('/')[2],
+          url: url,
+          css: selector,
+          lastChecked: 0,
+          lastUpdated: 0,
+          hash: 0
+        };
+
+        got(site.url).then(response => {
+          const dom = new JSDOM(response.body);
+
+          if (site.css)
+            var content = dom.window.document.querySelector(site.css).textContent;
+          else
+            var content = dom.window.document.querySelector('head').textContent;
+
+          var hash = crypto.createHash('md5').update(content).digest('hex');
+
+          var time = new Date();
+          site.lastChecked = time.toLocaleString();
+          site.lastUpdated = time.toLocaleString();
+          site.hash = hash;
+
+          sitesToMonitor.push(site);
+          console.log(sitesToMonitor);
+
+          fs.outputJSON(file, sitesToMonitor, { spaces: 2 }, err => {
+            if (err) console.log(err)
+          });
+
+          var embed = new Discord.MessageEmbed();
+          embed.addField(`Site added:`, `Name: ${site.id}\nURL: ${site.url}\nCSS: \`${site.css}\`\n`)
+          embed.setColor('0x6058f3');
+          message.channel.send(embed);
+
+        }).catch(err => {
+          return message.channel.send(`Error: \`${err}\``);
+        });
+      } break;
+    case "REMOVE":
+      {
+        if (args.length === 0 || isNaN(args[0])) return message.channel.send('Usage: `!remove <NR [1-99]>`');
+        if (args[0] < 1 || args[0] > 99 || args[0] > sitesToMonitor.length) return message.channel.send('Not a valid number. Usage: `!remove <NR [1-99]>`');
+
+        const id = sitesToMonitor[args[0] - 1].id;
+        sitesToMonitor.splice(args[0] - 1, 1);
+
+        fs.outputJSON(file, sitesToMonitor, { spaces: 2 }, err => {
+          if (err) console.log(err)
+        })
+
+        console.log(sitesToMonitor);
+        message.channel.send(`Removed **${id}** from list.`);
+
+      } break;
+    case "LIST":
+      {
+        if (sitesToMonitor.length < 1) return message.channel.send('No sites to monitor. Add one with `!add`.');
+
+        var embed = new Discord.MessageEmbed();
+        for (let i = 0; i < sitesToMonitor.length; i++) {
+          embed.setTitle(`${sitesToMonitor.length} site(s) being monitored:`);
+          embed.addField(`${sitesToMonitor[i].id}`, `URL: ${sitesToMonitor[i].url}\nCSS: \`${sitesToMonitor[i].css}\`\nChecked: ${sitesToMonitor[i].lastChecked}\nUpdated: ${sitesToMonitor[i].lastUpdated}\nRemove: \`!remove ${i + 1}\``);
+          embed.setColor('0x6058f3');
+        }
+
+        message.channel.send(embed);
+
+      } break;
+    case "UPDATE":
+      {
+        message.channel.send(`Updating \`${sitesToMonitor.length}\` site(s)...`);
+        update();
+        message.channel.send(`Done...`);
+      } break;
+    case "INTERVAL":
+      if (args.length === 0 || isNaN(args[0]) || args[0] < 1 || args[0] > 60) return message.channel.send('Usage: `!interval <MINUTES [1-60]>`');
+      {
+        if (Math.round(args[0]) < 60) {
+          cronUpdate.setTime(new CronTime(`0 */${Math.round(args[0])} * * * *`));
+        } else {
+          cronUpdate.setTime(new CronTime(`0 0 * * * *`));
+        }
+        cronTime.interval = Math.round(args[0]);
+
+        fs.outputJSON(settingsFile, cronTime, { spaces: 2 }, err => {
+          if (err) console.log(err)
+        });
+
+        message.channel.send(`Interval set to \`${cronTime.interval}\` minutes.`);
+        cronUpdate.start();
+      } break;
+    case "START":
+      {
+        cronUpdate.start();
+        var time = new Date();
+        console.log(`Cron started at ${time.toLocaleString()}`);
+        message.channel.send(`Started monitoring...`);
+      } break;
+    case "STOP":
+      {
+        cronUpdate.stop();
+        var time = new Date();
+        console.log(`Cron stopped at ${time.toLocaleString()}`);
+        message.channel.send('Paused website monitoring... Type `!start` to resume.');
+      } break;
+    case "STATUS":
+      {
+        var time = new Date();
+        console.log('Status: ', cronUpdate.running);
+        if (cronUpdate.running) message.channel.send(`Site Watcher is running with an interval of \`${cronTime.interval}\` minute(s).`);
+        else message.channel.send('Site Watcher is not running. Use `!start` to start monitoring websites.');
+      } break;
+    default:
+      message.channel.send('Invalid command...\nType `!help` for a list of commands.');
+  }
+})
+
+function update() {
+  let channel = client.channels.cache.get(process.env.DISCORDJS_TEXTCHANNEL_ID);
+
+  for (let i = 0; i < sitesToMonitor.length; i++) {
+    const url = sitesToMonitor[i].url;
+    got(url).then(response => {
+      const dom = new JSDOM(response.body);
+
+      if (sitesToMonitor[i].css)
+        var content = dom.window.document.querySelector(sitesToMonitor[i].css).textContent;
+      else
+        var content = dom.window.document.querySelector('head').textContent;
+
+      var hash = crypto.createHash('md5').update(content).digest('hex');
+
+      var time = new Date();
+      sitesToMonitor[i].lastChecked = time.toLocaleString();
+
+      if (sitesToMonitor[i].hash != hash) {
+        var prevUpdate = sitesToMonitor[i].lastUpdated;
+        sitesToMonitor[i].lastUpdated = time.toLocaleString();
+        sitesToMonitor[i].hash = hash;
+
+        var embed = new Discord.MessageEmbed();
+        embed.setTitle(`🔎 ${sitesToMonitor[i].id} changed!`);
+        embed.addField(`URL`, `${sitesToMonitor[i].url}`);
+        embed.addField(`CSS`, `\`${sitesToMonitor[i].css}\``);
+        embed.addField(`Previous change`, `${prevUpdate}`, true);
+        embed.addField(`Updated on`, `${sitesToMonitor[i].lastUpdated}`, true);
+        embed.setColor('0x6058f3');
+
+        channel.send(embed);
+
+        fs.outputJSON(file, sitesToMonitor, { spaces: 2 }, err => {
+          if (err) console.log(err)
+        });
+      }
+    }).catch(err => {
+      return console.log(`Error: ${err}`);
+    });
+  }
+}
+
+const cronUpdate = new CronJob(`0 */${cronTime.interval} * * * *`, function () {
+  var time = new Date();
+  console.log(`Cron executed at ${time.toLocaleString()}`);
+  update();
+}, null, false);
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
