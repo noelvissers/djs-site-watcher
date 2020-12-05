10de1d7a1fd230dc93b962d1d0cba713122160a6
Noël Vissers <noelvisser@gmail.com>
2020-12-05 14:14:43 +0100

    Add comments to code where needed

--- src/monitor.js
+++ src/monitor.js
@@ -20,41 +20,52 @@ const { JSDOM } = jsdom;
 var crypto = require('crypto');
 const fs = require('fs-extra');
 
-const PREFIX = '!';
+const PREFIX = '!'; //Change this to anything you like as a prefix
 var regexp = /[^\s"]+|"([^"]*)"/gi;
 const file = './src/sites.json';
 var sitesToMonitor = [];
 const settingsFile = './src/settings.json';
 var cronTime = { interval: 5 };
 
+//Events when bot comes online
 client.on('ready', () => {
+  //Load saved sites
   var tempJson = fs.readJSONSync(file);
   console.log(tempJson);
   sitesToMonitor = [...tempJson];
 
+  //Load saved settings
   tempJson = fs.readJSONSync(settingsFile);
   console.log(tempJson);
   cronTime = tempJson;
 
-  cronUpdate.setTime(new CronTime(`0 */${cronTime.interval} * * * *`));
+  //Start monitoring
+  if (cronTime.interval < 60)
+    cronUpdate.setTime(new CronTime(`0 */${cronTime.interval} * * * *`));
+  else
+    cronUpdate.setTime(new CronTime(`0 0 * * * *`));
   cronUpdate.start();
   console.log(`[${client.user.tag}] Ready...\n[${client.user.tag}] Running an interval of ${cronTime.interval} minute(s).`);
 })
 
+//Events on message
 client.on('message', (message) => {
+  //Check if message starts with prefix and remove prefix from string
   if (!message.content.startsWith(PREFIX) || message.author.bot) return;
   var args = [];
   console.log(`[${message.author.tag}]: ${message.content}`);
   const argsTemp = message.content.slice(PREFIX.length).trim();
 
+  //Split the string in command, and arguments. This part splits on spaces exept if it is between quotes ("a b")
   do {
     var match = regexp.exec(argsTemp);
     if (match != null) {
       args.push(match[1] ? match[1] : match[0]);
     }
   } while (match != null);
-
   console.log(args);
+
+  //Make command uppercase so !help and !Help both work (including all other commands)
   const CMD_NAME = args.shift().toLowerCase();
 
   switch (CMD_NAME.toUpperCase()) {
@@ -83,6 +94,7 @@ client.on('message', (message) => {
           selector = args[1];
         }
 
+        //Create site object
         var site = {
           id: url.split('/')[2],
           url: url,
@@ -92,34 +104,43 @@ client.on('message', (message) => {
           hash: 0
         };
 
+        //Check if site is valid
         got(site.url).then(response => {
           const dom = new JSDOM(response.body);
 
+          //Get css element
           if (site.css)
             var content = dom.window.document.querySelector(site.css).textContent;
+          //Get head is no css element is selected
           else
             var content = dom.window.document.querySelector('head').textContent;
 
+          //Hash the site content so only a short string is saved
           var hash = crypto.createHash('md5').update(content).digest('hex');
 
+          //Set time for added site
           var time = new Date();
           site.lastChecked = time.toLocaleString();
           site.lastUpdated = time.toLocaleString();
           site.hash = hash;
 
+          //Add site to site array
           sitesToMonitor.push(site);
           console.log(sitesToMonitor);
 
+          //Save updated array to file
           fs.outputJSON(file, sitesToMonitor, { spaces: 2 }, err => {
             if (err) console.log(err)
           });
 
+          //Send confirmation message
           var embed = new Discord.MessageEmbed();
           embed.addField(`Site added:`, `Name: ${site.id}\nURL: ${site.url}\nCSS: \`${site.css}\`\n`)
           embed.setColor('0x6058f3');
           message.channel.send(embed);
 
         }).catch(err => {
+          //Return any errors that might occur, like invalid site/css
           return message.channel.send(`Error: \`${err}\``);
         });
       } break;
@@ -202,29 +223,37 @@ client.on('message', (message) => {
   }
 })
 
+//Update the sites
 function update() {
   let channel = client.channels.cache.get(process.env.DISCORDJS_TEXTCHANNEL_ID);
 
   for (let i = 0; i < sitesToMonitor.length; i++) {
     const url = sitesToMonitor[i].url;
     got(url).then(response => {
+      //Get content of site
       const dom = new JSDOM(response.body);
 
+      //If css is selected, only get that part
       if (sitesToMonitor[i].css)
         var content = dom.window.document.querySelector(sitesToMonitor[i].css).textContent;
+      //If no css is selected get the whole head
       else
         var content = dom.window.document.querySelector('head').textContent;
 
+      //hash the content of the site, so only a short string is saved
       var hash = crypto.createHash('md5').update(content).digest('hex');
 
+      //Update the time for when the last check was
       var time = new Date();
       sitesToMonitor[i].lastChecked = time.toLocaleString();
 
+      //Check if new has differs from last hash
       if (sitesToMonitor[i].hash != hash) {
         var prevUpdate = sitesToMonitor[i].lastUpdated;
         sitesToMonitor[i].lastUpdated = time.toLocaleString();
         sitesToMonitor[i].hash = hash;
 
+        //Send update to Discord channel
         var embed = new Discord.MessageEmbed();
         embed.setTitle(`🔎 ${sitesToMonitor[i].id} changed!`);
         embed.addField(`URL`, `${sitesToMonitor[i].url}`);
@@ -232,9 +261,9 @@ function update() {
         embed.addField(`Previous change`, `${prevUpdate}`, true);
         embed.addField(`Updated on`, `${sitesToMonitor[i].lastUpdated}`, true);
         embed.setColor('0x6058f3');
-
         channel.send(embed);
 
+        //Save the new data in the file
         fs.outputJSON(file, sitesToMonitor, { spaces: 2 }, err => {
           if (err) console.log(err)
         });
@@ -245,6 +274,7 @@ function update() {
   }
 }
 
+//Update on set interval
 const cronUpdate = new CronJob(`0 */${cronTime.interval} * * * *`, function () {
   var time = new Date();
   console.log(`Cron executed at ${time.toLocaleString()}`);
